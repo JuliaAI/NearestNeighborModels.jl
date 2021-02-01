1fe298d3710eda5538bcffef028e854f69c49e86
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2021-02-01 23:03:55 +0100

    rm export of stray KnnKernel

--- src/MLJNearestNeighborsInterface.jl
+++ src/MLJNearestNeighborsInterface.jl
@@ -19,7 +19,7 @@ using Statistics
 export list_kernels, ColumnTable, DictTable
 
 # Export KNN models
-export KNNClassifier, KNNRegressor, KnnKernel, MultitargetKNNRegressor
+export KNNClassifier, KNNRegressor, MultitargetKNNRegressor
 
 # Re-Export Distance Metrics from `Distances.jl`
 export Euclidean, Cityblock, Minkowski, Chebyshev, Hamming, WeightedEuclidean,
