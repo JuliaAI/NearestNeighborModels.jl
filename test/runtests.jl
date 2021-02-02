3ca0e3446ba1e84239e9a2ed66e71062d4082c75
Okon Samuel <39421418+OkonSamuel@users.noreply.github.com>
2021-02-02 01:07:17 +0100

    update tests to reflect name change

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,10 @@
-using MLJNearestNeighborsInterface
+using NearestNeighborsModels
 using StableRNGs
 using Test
 using MLJBase
 using OffsetArrays
 
-import MLJNearestNeighborsInterface: check_onebased_indexing, combine_weights,
+import NearestNeighborsModels: check_onebased_indexing, combine_weights,
     err_if_given_invalid_K, Fill, get_weights, KNNResult, NeighDistsMatrix,
     NeighIndsMatrix, RankOneMatrix, _replace!, scale, sort_idxs, _sum
 
